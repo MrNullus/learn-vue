164ab94ef07641795dabc78746d87391c0a3689f
Mr. Punk Da Silva <gustavojs417@gmail.com>
2024-09-05 19:41:15 -0300

    add

---
+++ modulo-1/projeto2_cli/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>
+    Hello World
+  </h1>
+
+  <PrimeiroComponent/>
+</template>
+
+<script>
+import PrimeiroComponent from "./components/PrimeiroComponent.vue";
+
+export default {
+  name : 'App',
+  components : {
+    PrimeiroComponent
+  }
+}
+</script>
---
+++ modulo-1/projeto2_cli/src/components/PrimeiroComponent.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name : "PrimeiroComponent",
+  data() {
+    return {
+      dado : "jogando dados no compoente",
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>
+    Usando dados no componente {{ dado }}
+  </h2>
+  <p>
+    Atualizando dados do componente...
+  </p>
+</template>
+
+<style scoped>
+
+</style>
